e668fe0eda549654f10579960933f1c8c2ee04cf
박주영 <juyoung7018.park@gmail.com>
2020-07-23 15:44:10 +0900

    Remove duplicate file in muklog app

--- map/views.py
+++ map/views.py
@@ -8,7 +8,9 @@ from blog.models import Blog
 
 
 def home(request):
-    muk_logs = Blog.objects.all().filter(user=request.user)
+    muk_logs = []
+    if request.user.is_authenticated:
+        muk_logs = Blog.objects.all().filter(user=request.user)
     context = {"muk_logs": muk_logs, "STATIC_URL": settings.STATIC_URL,
                "naver_client_id": settings.NAVER_CLIENT_ID}
     return HttpResponse(render(request, "home.html", context))
--- muklog/map/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views as map
-
-urlpatterns = [
-    path('', map.home, name="home")
-]
