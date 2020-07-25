39c57d921fd284d7cc7cfe9f064b5e245bf44ba2
박주영 <juyoung7018.park@gmail.com>
2020-07-25 22:04:03 +0900

    Excute collectstatic command

--- muklog/settings.py
+++ muklog/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'muklog', 'static'),
     os.path.join(BASE_DIR, 'map', 'static'),
     os.path.join(BASE_DIR, 'blog', 'static')
 ]
---
+++ static/map/js/ClusteredMap.js
@@ -0,0 +1,51 @@
+import NaverMap from "../../shared/js/NaverMap.js";
+import { getClusterImg } from "../../shared/js/shared.js";
+import { MarkerClustering } from "../../shared/js/markerCluster.js";
+import LogMarker from "../js/LogMarker.js";
+
+class ClusteredMap extends NaverMap {
+  constructor(staticUrl, sidePost, isLoggedIn) {
+    super(staticUrl);
+    this.sidePost = sidePost;
+    this.isLoggedIn = isLoggedIn;
+  }
+
+  setClustering(mukLogs) {
+    this.markers = [];
+    mukLogs.forEach((muklog) => {
+      const position = {
+        latitude: muklog.latitude,
+        longitude: muklog.longitude,
+      };
+      const marker = new LogMarker(position, muklog, this.map, this.sidePost);
+      marker.setLogInfo();
+      this.markers.push(marker.marker);
+    });
+
+    const markerImg = getClusterImg(this.staticUrl);
+
+    new MarkerClustering({
+      minClusterSize: 2,
+      maxZoom: 8,
+      map: this.map,
+      markers: this.markers,
+      disableClickZoom: false,
+      gridSize: 120,
+      icons: [
+        markerImg.marker1,
+        markerImg.marker2,
+        markerImg.marker3,
+        markerImg.marker4,
+        markerImg.marker5,
+      ],
+      indexGenerator: [10, 100, 200, 500, 1000],
+      stylingFunction: function (clusterMarker, count) {
+        let cluster = clusterMarker.getElement();
+        let child = cluster.childNodes;
+        child[0].innerText = count;
+      },
+    });
+  }
+}
+
+export default ClusteredMap;
